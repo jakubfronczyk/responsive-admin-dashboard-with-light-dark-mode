99e49007811a2292e8ecac3e7cdd9affb7c83b96
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-12 15:40:50 +0200

    all features have been added

--- app.js
+++ app.js
@@ -20,3 +20,18 @@ themeToggler.addEventListener("click",() => {
     themeToggler.querySelector('span:first-child').classList.toggle('active');
     themeToggler.querySelector('span:last-child').classList.toggle('active');
 })
+
+//fill orders in table
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'Pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Details</td>
+                    `
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
