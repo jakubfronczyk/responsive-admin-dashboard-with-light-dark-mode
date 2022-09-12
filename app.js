fac6fa0ec43802c8aa43d5ffa88ec308bc3df55c
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-12 15:18:21 +0200

    added changing theme function

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 
+const themeToggler = document.querySelector(".theme-toggler")
+
+
+//show sidebar
 menuBtn.addEventListener("click",() => {
     sideMenu.classList.toggle("active");
 })
-
+//close sidebar
 closeBtn.addEventListener("click",() => {
     sideMenu.classList.toggle("active");
+})
+
+//change theme
+themeToggler.addEventListener("click",() => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span:first-child').classList.toggle('active');
+    themeToggler.querySelector('span:last-child').classList.toggle('active');
 })
