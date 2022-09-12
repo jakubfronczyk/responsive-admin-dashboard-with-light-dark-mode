93c59cf3ec3a262e9ed0a7fbf2bb438e432cb0f9
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-09-12 15:27:57 +0200

    array of orders

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with Camera Dron',
+        productNumber: '54238',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k DRON',
+        productNumber: '98452',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJJ Air 2S',
+        productNumber: '22135',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozange Drone',
+        productNumber: '43213',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
